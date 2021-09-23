af724ab004a5009cc18c7f0afa3188d96def56b2
student <your@example.com>
2021-09-23 12:04:59 +0200

    prvy greenfoot

---
+++ Jablko.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Jablko here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Jablko extends Actor
+{
+
+}
---
+++ kvet.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class kvet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class kvet extends Actor
+{
+    /**
+     * Act - do whatever the kvet wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
